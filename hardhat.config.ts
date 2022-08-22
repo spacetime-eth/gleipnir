020d6afb887cc4d50155fac9096c1b49e4b4b7c6
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-21 22:25:15 -0300

    add deploy capacity for local network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+	networks: {
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			chainId: 31337
+		}
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,13 @@
 import { ethers } from "hardhat";
 
-async function main() {}
+async function main() {
+	const BoardManager = await ethers.getContractFactory("BoardManager")
+
+	console.log("Deploying... ")
+	const contract = await BoardManager.deploy()
+	await contract.deployed()
+	console.log(`Deployed to ${contract.address}`)
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
