f51c7cf22fd5425411aa989fed8be5d5954aed0e
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-09-23 17:52:21 -0300

    chaos commit: add partial assignation logic for drawings

--- contracts/BoardManager.sol
+++ contracts/BoardManager.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+
+struct Drawing {
+    address owner;
+    uint256[] image;
+}
+
 contract BoardManager {
     enum Status {
         Idle,
@@ -8,13 +14,14 @@ contract BoardManager {
         Finished
     }
 
-    uint256[16] NO_DRAWING = [
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0
-    ];
-    uint256[16][] board;
+    uint256[16] NO_DRAWING;
+
+    Drawing[] drawings;
+    uint32 first_assignable;
+    uint32 last_assignable;
+    uint32 ring;
+    uint32 rings_offset;
+
     Status status = Status.Idle;
 
     function start() public {
@@ -22,17 +29,48 @@ contract BoardManager {
         status = Status.Started;
     }
 
+    function reserveCanvas() {
+        for (uint i = first_assignable; i < last_assignable; i++) {
+            Drawing drawing = drawings[i];
+            if (drawings.owner == address(0x0) && isEmptyDrawing(drawing.image)) {
+                drawings[i] = Drawing(msg.sender, timestamp, NO_DRAWING);
+
+                    //TODO timestamp
+                break;
+            }
+        }
+    }
+
+    function getMyCanvas() view public returns (uint256[16] memory) {
+        for (uint i = first_assignable; i < last_assignable; i++) {
+            if (drawings[i].owner == msg.sender) {
+                return drawings[i].image;
+            }
+        }
+        return NO_DRAWING;//TODO maybe fail instead?
+    }
+
     function getCanvas() view public returns (uint256[16][4] memory) {
         require(status == Status.Started, "Board must be started before getting a canvas");
-        uint256[16] memory last = board.length > 0 ? board[board.length - 1] : NO_DRAWING;
+        uint256[16] memory last = drawings.length > 0 ? drawings[drawings.length - 1].image : NO_DRAWING;
         uint256[16][4] memory result = [NO_DRAWING, NO_DRAWING, NO_DRAWING, last];
         return result;
     }
 
     function draw(uint256[16] calldata drawing) public {
         require(status == Status.Started, "Board must be started before drawing");
-        require(isNotEmpty(drawing), "Drawing shouldn't be empty");
-        board.push(drawing);
+        require(!isEmptyDrawing(drawing), "Drawing shouldn't be empty");
+
+        for (uint i = first_assignable; i < last_assignable; i++) {
+            if (drawings[i].owner == msg.sender) {
+                drawing[i].image = drawing;
+                if (i == first_assignable) {
+                    //TODO calculate first assignable
+                }
+                break;
+            }
+        }
+        return NO_DRAWING;
     }
 
     function finish() public {
@@ -40,12 +78,12 @@ contract BoardManager {
         status = Status.Finished;
     }
 
-    function isNotEmpty(uint256[16] calldata drawing) private pure returns(bool) {
+    function isEmptyDrawing(uint256[16] calldata drawing) private pure returns(bool) {
         for (uint i = 0; i < 16;) {
             if (drawing[i] > 0)
-                return true;
+                return false;
             unchecked { i += 1; }
         }
-        return false;
+        return true;
     }
 }
